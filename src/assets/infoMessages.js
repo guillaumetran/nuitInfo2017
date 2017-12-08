03510111a56a027aef16208bb41359199509fda3
Guillaume Tran <guillaume.tran@epitech.eu>
2017-12-08 08:06:21 +0100

    Update scenario

--- src/assets/infoMessages.js
+++ src/assets/infoMessages.js
@@ -5,7 +5,7 @@ module.exports = [
   {
     _id: 1,
     text:
-      "Vous avez rencontré Eva lors d'une soirée chez Guillaume, vous l'ajouté le lendemain pour apprendre à la connaitre",
+      "Vous avez rencontré Eva lors d'une soirée chez Guillaume, vous l'avez ajouté le lendemain pour apprendre à la connaitre",
     nextMessage: 1
   }
 ];
