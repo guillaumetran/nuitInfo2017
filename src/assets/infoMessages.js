e6bdc7fc4172cf5c490e7dc05f6e30d1b6eb7e3b
Guillaume Tran <guillaume.tran@epitech.eu>
2017-12-08 07:38:53 +0100

    Update scenario

--- src/assets/infoMessages.js
+++ src/assets/infoMessages.js
@@ -5,15 +5,7 @@ module.exports = [
   {
     _id: 1,
     text:
-      "Vous avez rencontré Eva lors d'une soirée chez Guillaume, vous l'ajoutée le lendemain pour apprendre à la connaitre",
+      "Vous avez rencontré Eva lors d'une soirée chez Guillaume, vous l'ajouté le lendemain pour apprendre à la connaitre",
     nextMessage: 1
-  },
-  {
-    _id: 2,
-    text: "Are you building a chat app?"
-  },
-  {
-    _id: 3,
-    text: "OKI"
   }
 ];
